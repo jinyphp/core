919b0ab21b78a612f2c1e2b48b833c0193d96ac0
hojinlee <infohojin@gmail.com>
2018-05-19 17:08:42 +0900

    init

---
+++ src/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jiny\Core;
+
+class Controller
+{
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        echo __CLASS__." 객체가 생성이 되었습니다.<br>";
+    }
+
+}
